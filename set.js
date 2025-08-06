314f68a1e85d2b63690c953285b194ad77deea10
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-08-06 21:54:26 +0300

    Create set.js

---
+++ set.js
@@ -0,0 +1,37 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+if (fs.existsSync('set.env')) {
+    require('dotenv').config({ path: path.join(__dirname, 'set.env') });
+}
+
+const config = {
+    
+    token: process.env.BOT_TOKEN || '7516471771:AAHyuyEkb7r4hV125yQ8Gy74DorGix67WH8',
+    owner_id: process.env.OWNER_ID || "7516471771",
+    prefix: process.env.PREFIX || ".",
+    timezone: process.env.TIMEZONE || "Africa/Nairobi",
+    greetNewMembers: {
+        enabled: process.env.GREET_ENABLED !== 'false',
+        gifUrl: process.env.GIF_URL || "https://files.catbox.moe/pm9x7c.gif"
+    },
+  
+    antiLink: {
+        enabled: process.env.ANTILINK !== 'false'
+    },
+    
+    url: process.env.URL || "https://files.catbox.moe/4h8lfw.jpg",
+    githubUrl: process.env.GITHUB_URL || "https://github.com/Keithkeizzah",
+    ownerName: process.env.OWNER_NAME || 'Keith',
+    botName: process.env.BOT_NAME || 'T-BOT'
+};
+
+const currentFile = require.resolve(__filename);
+fs.watchFile(currentFile, () => {
+    fs.unwatchFile(currentFile);
+    console.log(`Updating ${path.basename(__filename)}`);
+    delete require.cache[currentFile];
+    require(currentFile);
+});
+
+module.exports = config;
